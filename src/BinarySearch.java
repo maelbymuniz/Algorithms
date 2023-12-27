84b4a224b6886ed53f780c767cb2985e3d5e4c04
Maelby Muniz <maelbymuniz@gmail.com>
2023-12-27 20:08:19 -0300

    Feat: BinarySearch works!

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch {
 
     public static void main(String[] args) {
 
-        System.out.println("test");
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        for (int i = 0; i < 2000; i++) {
+            numeros.add(i);
+        }
+
+        System.out.println("Quantidade de tentativas:" +  BuscaBinaria(numeros, 300));
+
+    }
+
+    private static Integer BuscaBinaria(ArrayList<Integer> numeros, int procurarPor) {
+        int chute;
+        int fim;
+        int meio;
+        int inicio = 0;
+        fim = numeros.size() -1;
+        int contador = 0;
+
+        while (inicio <= fim) {
+            meio = (inicio + fim) / 2;
+            chute = numeros.get(meio);
+
+            if (chute == procurarPor) {
+               return contador;
+            } else if (chute > procurarPor) {
+                fim = meio -1;
+            } else {
+                inicio = meio + 1;
+            }
+            contador++;
+        }
+        return contador;
     }
 }
